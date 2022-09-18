ffccac869d38cb9ee9fefd645b543cfd9d444a5f
dengwenj <927561237@qq.com>
2022-09-18 20:42:08 +0800

    使用styled-components

--- guaixiangai/src/pages/home/index.tsx
+++ guaixiangai/src/pages/home/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Button } from 'antd'
+
+import { HomeWrapper } from './styled'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <HomeWrapper>
+      <h2>怪像哎</h2>
+      <div>
+        <Button>简单模式</Button>
+        <Button>一般模式</Button>
+        <Button>困难模式</Button>
+        <Button>羊了个羊模式</Button>
+      </div>
+    </HomeWrapper>
   )
 }
