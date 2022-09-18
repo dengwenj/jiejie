8b698f39ef0c6a47c3b2e409e9214c49a0a29881
dengwenj <927561237@qq.com>
2022-09-18 20:15:45 +0800

    404

--- guaixiangai/src/pages/notfount/index.tsx
+++ guaixiangai/src/pages/notfount/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Result } from 'antd'
 
 export default function NotFount() {
+  const navigate = useNavigate()
+
   return (
-    <div>NotFount</div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，你访问的页面不存在"
+      extra={<Button 
+        type="primary" 
+        onClick={() => navigate('/')}
+      >
+        返回首页
+      </Button>}
+    />
   )
 }
