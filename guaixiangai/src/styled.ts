e5fb81534b5688ab071e2d90f628b36413453f8f
dengwenj <927561237@qq.com>
2022-09-18 23:33:21 +0800

    背景图

--- guaixiangai/src/styled.ts
+++ guaixiangai/src/styled.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
 
+import bgi from './assets/images/bgi.jpeg'
+
 export const AppWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
   display: flex;
   justify-content: center;
   padding-top: 50px;
+  background: url(${bgi});
+  background-size: cover;
 `
