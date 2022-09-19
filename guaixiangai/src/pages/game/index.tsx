7ff8b997c3906f22093ace16b259c4f48ca7b18c
dengwenj <927561237@qq.com>
2022-09-19 23:29:49 +0800

    xiuga

--- guaixiangai/src/pages/game/index.tsx
+++ guaixiangai/src/pages/game/index.tsx
@@ -1,12 +1,62 @@
+import { useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
+
+import { GameWrapper, ItemBlock } from './styled'
 
 export default function Game() {
   const location = useLocation()
-  console.log(location);
+  const blockRef = useRef(null)
+
+  useEffect(() => {
+    const el = blockRef.current! as HTMLElement
+    console.log(el.previousSibling)
+    console.log(el.nextElementSibling)
+    
+  }, [])
+
+  /**
+   * 处理函数
+   */
+  // 点击每一块
+  const handleClickItemBlock = (num: number) => {
+    console.log('点击了', num)
+  }
   
   return (
-    <div>
-      Game
-    </div>
+    <GameWrapper>
+      <ItemBlock
+        ref={blockRef}
+        style={{
+          left: 230,
+          zIndex: 103,
+          backgroundColor: '#000'
+        }}
+        onClick={() => handleClickItemBlock(1)}
+      >🏆</ItemBlock>
+      <ItemBlock
+        style={{
+          left: 200,
+          top: 180,
+          zIndex: 102
+        }}
+        onClick={() => handleClickItemBlock(2)}
+      >🏆</ItemBlock>
+      <ItemBlock
+        style={{
+          left: 230,
+          top: 80,
+          zIndex: 101
+        }}
+        onClick={() => handleClickItemBlock(3)}
+      >🏆</ItemBlock>
+      <ItemBlock
+        style={{
+          left: 230,
+          top: 100,
+          zIndex: 100
+        }}
+        onClick={() => handleClickItemBlock(4)}
+      >🏆</ItemBlock>
+    </GameWrapper>
   )
 }
---
+++ guaixiangai/src/pages/game/styled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const GameWrapper = styled.div`
+
+`
+
+export const ItemBlock = styled.div`
+  position: absolute;
+  width: 45px;
+  height: 45px;
+  background-color: #eee;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 26px;
+`
